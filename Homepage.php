d5f61ff53ffe508d246bd5779933f5e54cce2d2c
Sakib Ali <sakibc157@gmail.com>
2021-07-29 01:07:01 +0600

    AJAX

---
+++ Homepage.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Home Page</title>
+</head>
+<body>
+    <h1> Welcome To Home Page</h1>
+    <a href="userlist.php">User List</a><br>
+    <a href="Logout.php">Logout</a>
+</body>
+</html>
